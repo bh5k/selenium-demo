11b4c1c1670ae2011d8edb9f97cde8122782dada
Bryan Hansen <bryankhansen@gmail.com>
2025-05-09 21:58:08 -0600

    adding size functionality

---
+++ js/pie-options.js
@@ -0,0 +1,5 @@
+function logSizeSelection(pieId) {
+    const selectedSize = document.getElementById(`size-${pieId}`).value;
+    console.log(`Pie ID ${pieId} selected size: ${selectedSize}`);
+}
+  
